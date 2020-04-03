09a64a4acafdc036757626b2c826f2c4f2c893b0
Luca <mylince@gmail.com>
2020-04-03 18:48:56 +0200

    In progress last step

--- js/main.js
+++ js/main.js
@@ -2,56 +2,72 @@
 var source = $("#calendar-template").html();
 var template = Handlebars.compile(source);
 
-var thisMonth = moment('2018-01-01'); //Ho il mio mese, giorno e anno di partenza
-dayCalendar(thisMonth);//Mese di partenza
+var thisStartMonth = moment('2018-01-01'); //Ho il mio mese, giorno e anno di partenza
+   dayCalendar(thisStartMonth);//Mese di partenza
+   var monthNumber = parseInt(thisStartMonth.format('M')); //Trovo il numero del mese
+   console.log(monthNumber);
+   holidays(monthNumber);
 
 //Mese successivo
 $('.next').click(function(){
-   thisMonth.add(1, 'month'); //aggiungo un mese ad ogni click
-   dayCalendar(thisMonth);
-   holidays()
+   thisStartMonth.add(1, 'month'); //aggiungo un mese ad ogni click
+   var monthNumber = parseInt(thisStartMonth.format('M')); //Trovo il numero del mese
+   console.log(monthNumber);
+   dayCalendar(thisStartMonth);
+   holidays(monthNumber);
 });
 
 //Mese precedente
 $('.prev').click(function(){
-   thisMonth.add(-1, 'month'); //aggiungo un mese ad ogni click
-   dayCalendar(thisMonth);
-   holidays()
+   thisStartMonth.add(-1, 'month'); //aggiungo un mese ad ogni click
+   var monthNumber = parseInt(thisStartMonth.format('M')); //Trovo il numero del mese
+   console.log(monthNumber);
+   dayCalendar(thisStartMonth);
+   holidays(monthNumber);
 });
 
 //Funzione al click
 function dayCalendar(thisMonth){
     $('#day-after-day').empty()//.html('');
-    var standardDay = thisMonth.clone(); //Clono
+    var standardDay = thisMonth.clone(); //clone
     var thisMonthMonths = thisMonth.daysInMonth(); //Trovo il numero di giorni che ha il mio numero
     var monthName = thisMonth.format('MMMM'); //Trovo il nome del mese
     $('#nome-mese').text(monthName); //Metto il nome del mese corrispondente ad ogni click
 
     for (var i = 1; i <= thisMonthMonths; i++) {
         var dayInCalendar = {
-           day:i + '' + monthName
+           day:i + '' + monthName,
+           dayDate: standardDay.format('YYYY-MM-DD')
         }
-       var templateFinale = template(dayInCalendar);//popolo template con handlebars
-       $('#day-after-day').append(templateFinale);
+
+        var templateFinale = template(dayInCalendar);//popolo template con handlebars
+        $('#day-after-day').append(templateFinale);
+        standardDay.add(1, 'day'); //perchè parte da 0
     }
 };
 
-function holidays(){
+function holidays(monthNumber){
     $.ajax({
         url:'https://flynn.boolean.careers/exercises/api/holidays',
         data: {
             year:2018,
-            month:0
+            month: monthNumber
         },
         method: 'GET',
-        success: function(data){
-            var giorniFestivi = data.response; //Estrapolo i giorni
+        success: function(myCalendar){
+            var giorniFestivi = myCalendar.response; //Estrapolo i giorni
             for (var i = 0; i < giorniFestivi.length; i++) {
             var giornoFestivo = giorniFestivi[i]; //Estrapolo un girono singolo
 
-            var nomeFestivo = giornoFestivo.name; //Estrapolo nome festività
-            var dataFestivo = giornoFestivo.date; //Estrapolo data festività
+            var nameHoliday = giornoFestivo.name; //Estrapolo nome festività
+            var dateHoliday = giornoFestivo.date; //Estrapolo data festività
+            $('#day-after-day li[data-day="' + dateHoliday + '"]').addClass('festivo').append(' - ' + nameHoliday);
             }
         }
     })
 };
+
+
+//if (dateHoliday == dayDate) {
+    //$('#day-after-day).addClass('festivo').append(' - ' + nameHoliday);
+//}
